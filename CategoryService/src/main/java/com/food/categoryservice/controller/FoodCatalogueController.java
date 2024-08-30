2512bdded38f822f9c25729be6baaa9288d15434
sophearyrin1 <srin@miu.edu>
2024-08-29 19:20:03 -0500

    dockerizaton backend and frontend

--- CategoryService/src/main/java/com/food/categoryservice/controller/FoodCatalogueController.java
+++ CategoryService/src/main/java/com/food/categoryservice/controller/FoodCatalogueController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/food-delivery-app/catalogs")
+@CrossOrigin
 public class FoodCatalogueController {
 
     @Autowired
--- Restaurant/src/main/java/com/food/restaurant/controller/RestaurantController.java
+++ Restaurant/src/main/java/com/food/restaurant/controller/RestaurantController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/food-delivery-app/restaurants")
+@CrossOrigin
 public class RestaurantController {
 
     @Autowired
