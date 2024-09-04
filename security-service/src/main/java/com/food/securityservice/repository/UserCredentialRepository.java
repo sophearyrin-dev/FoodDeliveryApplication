d996fefa5fb2df284d57b5256f29876bb4d39f54
sophearyrin1 <srin@miu.edu>
2024-09-03 23:40:03 -0500

    user authentication

---
+++ security-service/src/main/java/com/food/securityservice/repository/UserCredentialRepository.java
@@ -0,0 +1,13 @@
+package com.food.securityservice.repository;
+
+
+import com.food.securityservice.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserCredentialRepository  extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
