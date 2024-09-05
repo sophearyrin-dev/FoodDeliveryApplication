e2e7e77a32ffab58449a2ea2d6e81ec1cdc83ca8
sophearyrin1 <srin@miu.edu>
2024-09-05 15:14:04 -0500

    authenticate request with api gateway

--- api-gateway/src/main/java/com/food/apigateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/food/apigateway/util/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -14,9 +15,8 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
+    @Value("${jwt.secret}")
+    private String SECRET;
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
